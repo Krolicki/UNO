82e38d8ee1fa336071930bf1c417fbf52489afe0
Jakub Królicki <101356174+Krolicki@users.noreply.github.com>
2022-03-20 21:38:35 +0100

    Wybranie gracza, plik js
    Dodanie możliwości wybrania gracza, przeniesienie skryptu do osobnego pliku.

---
+++ script.js
@@ -0,0 +1,29 @@
+var player;
+var num = document.getElementsByClassName('num');
+function disableForm() {
+    for (var i = 0; i < num.length; i++) {
+         num[i].style.pointerEvents = 'none';
+    }
+    let points = document.getElementById(player);
+    points.innerHTML = parseInt(points.innerHTML) + parseInt(document.dodaj.suma.value);
+    document.dodaj.suma.value = 0;
+    document.getElementById('first').style.background = "rgba(255,255,255,.5)";
+    document.getElementById('second').style.background = "rgba(255,255,255,.5)";
+}
+
+function enableForm(pla){
+    player = pla;
+    for (var i = 0; i < num.length; i++) {
+         num[i].style.pointerEvents = 'auto';
+    }
+    switch(pla){
+        case 'points-first':
+            document.getElementById('first').style.background = "rgba(0,255,0,.5)";
+            document.getElementById('second').style.background = "rgba(255,255,255,.5)";
+            break;
+        case 'points-second':
+            document.getElementById('second').style.background = "rgba(0,255,0,.5)";
+            document.getElementById('first').style.background = "rgba(255,255,255,.5)";
+            break;
+    }
+} 
